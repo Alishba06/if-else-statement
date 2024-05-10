61e34b7a9c26928036b8c2969f6e7f4d7187a2df
A06 <160205029+Alishba06@users.noreply.github.com>
2024-05-10 16:12:06 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,76 @@
+// let dayOff = "Sunday";
+
+// switch (dayOff) {
+//     case (dayOff):
+//         console.log("Go hiking");
+//         break;
+
+//     case "Sunday":
+//         console.log("Read a book");
+//         break;
+//         fault:
+//         console.log("work on a hobby.");    
+// }
+
+
+// function greet(){
+//     console.log("hello world!")
+//     console.log("how are you!")
+//     console.log("hello world!")
+//     console.log("hello world!")
+// }
+
+// greet()
+// greet()
+// greet()
+// greet()
+
+// function addNumber(num1: number, num2: number) {
+//      const sum = num1 + num2;
+//      console.log("sum:", sum);
+// }
+
+// addNumber(5 , 3)
+
+
+
+// function addfunction(num: number){
+
+//     console.log(num+" x  1 = "+ num*1)
+//     console.log(num+" x  2 = "+ num*2)
+//     console.log(num+" x  3 = "+ num*3)
+//     console.log(num+" x  4 = "+ num*4)
+//     console.log(num+" x  5 = "+ num*5)
+//     console.log(num+" x  6 = "+ num*6)
+//     console.log(num+" x  7 = "+ num*7)
+//     console.log(num+" x  8 = "+ num*8)
+//     console.log(num+" x  9 = "+ num*9)
+//     console.log(num+" x  10 = "+ num*10)
+        
+// }
+
+// addfunction(2);
+// console.log("")
+// addfunction(3);
+// console.log("");
+
+
+function greet(personname: string){
+    console.log("hello my name is" +" "+ personname)
+    return personname
+}
+
+let myname ="aliza"
+
+// greet("aliza");
+// greet("fatima")
+
+greet(myname);
+
+console.log(myname +" "+ "is my name ")
+console.log(myname +" "+ " alishba and i am a")
+console.log(myname + " student ")
+
+
+
+
